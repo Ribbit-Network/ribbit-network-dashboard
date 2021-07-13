0d76a6de92f2f6085862b5bef2fd571437d3b1c6
Keenan Johnson <keenan.johnson@gmail.com>
2021-07-13 11:07:44 -0700

    Updated with battery graphs

--- app.py
+++ app.py
@@ -24,9 +24,9 @@ query_api = client.query_api()
 def get_influxdb_data():
     ## Query data as pandas dataframe
     data_frame = query_api.query_data_frame('from(bucket:"co2") '
-                                            '|> range(start: -1h) '
+                                            '|> range(start: -6h) '
                                             '|> pivot(rowKey:["_time"], columnKey: ["_field"], valueColumn: "_value") '
-                                            '|> keep(columns: ["co2", "temperature", "humidity", "lat", "lon", "alt", "_time"])')
+                                            '|> keep(columns: ["co2", "temperature", "humidity", "lat", "lon", "alt", "_time", "solar_power_watt", "batt_bus_v"])')
 
     return data_frame
 
@@ -34,45 +34,43 @@ def serve_layout():
     #Define this function to query new data on page load
     return html.Div([
         html.H1("CO2 PPM Database"),
-        html.Div([
-            html.Div([
-                html.H3('Sensor Map'),
-                dcc.Graph(
-                    id='co2_globe',
-                    figure=globe_fig,
-                    style={
-                    "width": "100%",
-                    'display': 'inline-block'
-                }),
-            ], className="six columns"),
-            html.Div([
-                html.H3('Sensor Data'),
-                dcc.Graph(
-                    id='co2_graph',
-                    figure=fig)
-            ], className="six columns"),
-            dcc.Interval(
-                id='interval-component',
-                interval=60*1000, # in milliseconds
-                n_intervals=0
-            )
-        ], className="row"),
         html.A(html.Button('Learn More!'),
-            href='https://github.com/keenanjohnson/ghg-gas-cloud',
+            href='https://github.com/keenanjohnson/ghg-gas-cloud'),
+        html.H3('Sensor Map'),
+        dcc.Graph(
+            id='co2_globe',
+            figure=globe_fig,
             style={
-                    "align-items": "center",
-                })
+            "width": "100%",
+            'display': 'inline-block'
+        }),
+        html.H3('Sensor Data'),
+        dcc.Graph(
+            id='co2_graph',
+            figure=co2_fig),
+        dcc.Graph(
+            id='batt_graph',
+            figure=batt_fig),
+        dcc.Graph(
+            id='solar_graph',
+            figure=solar_fig),
+        dcc.Interval(
+            id='interval-component',
+            interval=60*1000, # in milliseconds
+            n_intervals=0),
     ])
 
 ## Query data as pandas dataframe
-fig = px.line(get_influxdb_data(), x="_time", y="co2", title="Co2 PPM")
+co2_fig = px.line(get_influxdb_data(), x="_time", y="co2", title="Co2 PPM")
+solar_fig = px.line(get_influxdb_data(), x="_time", y="solar_power_watt", title="Solar Power (Watts)")
+batt_fig = px.line(get_influxdb_data(), x="_time", y="batt_bus_v", title="Battery Voltage")
 
 # Only get the latest point for displaying on the map
 map_df = query_api.query_data_frame('from(bucket:"co2") '
                                     '|> range(start:-15m) '
                                     '|> limit(n:1) '
                                     '|> pivot(rowKey:["_time"], columnKey: ["_field"], valueColumn: "_value") '
-                                    '|> keep(columns: ["co2", "temperature", "humidity", "lat", "lon", "alt", "_time"])')
+                                    '|> keep(columns: ["co2","lat", "lon"])')
 
 globe_fig =go.Figure(data=go.Scattergeo(
     lon = map_df['lon'],
@@ -94,18 +92,19 @@ globe_fig.update_geos(
 globe_fig.update_layout(height=500, margin={"r":0,"t":0,"l":0,"b":0})
 
 app.layout = serve_layout
-app.css.append_css({
-    'external_url': 'https://codepen.io/chriddyp/pen/bWLwgP.css'
-})
 
 # update temperature graph
 @app.callback(Output('co2_graph', 'figure'),
+              Output('solar_graph', 'figure'),
+              Output('batt_graph', 'figure'),
               [Input('interval-component', 'n_intervals')
               ])
 def update_graph(n):
     ## Query data as pandas dataframe
-    fig = px.line(get_influxdb_data(), x="_time", y="co2", title="Co2 PPM")
-    return fig
+    temp_fig = px.line(get_influxdb_data(), x="_time", y="co2", title="Co2 PPM")
+    solar_fig = px.line(get_influxdb_data(), x="_time", y="solar_power_watt", title="Solar Power (Watts)")
+    batt_fig = px.line(get_influxdb_data(), x="_time", y="batt_bus_v", title="Battery Voltage")
+    return temp_fig, solar_fig, batt_fig
 
 if __name__ == '__main__':
     app.run_server(debug=True)
