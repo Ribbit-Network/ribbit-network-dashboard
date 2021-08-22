f4331409d38121d14fff22438b869396bda5789d
Keenan Johnson <keenan.johnson@gmail.com>
2021-08-21 17:55:42 -0700

    Updated main sensor

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def get_influxdb_data():
     ## Query data as pandas dataframe
     data_frame = query_api.query_data_frame('from(bucket:"co2") '
                                             '|> range(start: -1h) '
-                                            '|> filter(fn: (r) => r.host == "af1ae06960bff131b214d73d7747d3b5")'
+                                            '|> filter(fn: (r) => r.host == "6cb1b8e43a19bdb3950a118a36af3452")'
                                             '|> pivot(rowKey:["_time"], columnKey: ["_field"], valueColumn: "_value") '
                                             '|> keep(columns: ["co2", "temperature", "humidity", "lat", "lon", "alt", "_time"])')
 
