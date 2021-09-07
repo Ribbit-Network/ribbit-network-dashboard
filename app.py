629fac98709c888de967a721c8e9293b5b804405
Keenan Johnson <keenan.johnson@gmail.com>
2021-09-06 20:02:39 -0700

    #1 Adds selector for sensor

--- app.py
+++ app.py
@@ -19,13 +19,15 @@ server = app.server
 client = InfluxDBClient.from_config_file("influx_config.ini")
 query_api = client.query_api()
 
-def get_influxdb_data(duration):
+
+def get_influxdb_data(duration, host):
     ## Query data as pandas dataframe
     df = query_api.query_data_frame('from(bucket:"co2")'
                                     f'|> range(start: -{duration})'
-                                    '|> filter(fn: (r) => r.host == "6cb1b8e43a19bdb3950a118a36af3452")'
+                                    f'|> filter(fn: (r) => r.host == "{host}")'
                                     '|> pivot(rowKey:["_time"], columnKey: ["_field"], valueColumn: "_value")'
                                     '|> keep(columns: ["co2", "temperature", "humidity", "lat", "lon", "alt", "_time", "baro_pressure"])')
+
     return df.drop(['result', 'table'], axis=1)
 
 def serve_layout():
@@ -38,6 +40,8 @@ def serve_layout():
             html.A(html.H3('Learn More'), href='https://ribbitnetwork.org/', style={'margin-left': 'auto', 'text-decoration': 'none', 'color': 'black'}),
         ], id='nav'),
 
+        dcc.Graph(id='map', figure=map_fig),
+
         dcc.Dropdown(id='duration', clearable=False, searchable=False, value='1h', options=[
             {'label': '10 minutes', 'value': '10m'},
             {'label': '30 minutes', 'value': '30m'},
@@ -45,7 +49,10 @@ def serve_layout():
             {'label': '1 day',      'value': '24h'},
         ]),
 
-        dcc.Graph(id='map', figure=map_fig),
+        dcc.Dropdown(id='host', clearable=False, searchable=False, value='6cb1b8e43a19bdb3950a118a36af3452', options=[
+            {'label': 'sensor 1', 'value': '6cb1b8e43a19bdb3950a118a36af3452'},
+            {'label': 'sensor 2', 'value': 'af1ae06960bff131b214d73d7747d3b5'},
+        ]),
 
         html.Div([
             dcc.Graph(id='co2_graph'),
@@ -63,8 +70,7 @@ map_df = query_api.query_data_frame('from(bucket:"co2") '
                                     '|> range(start:-15m) '
                                     '|> limit(n:1) '
                                     '|> pivot(rowKey:["_time"], columnKey: ["_field"], valueColumn: "_value") '
-                                    '|> keep(columns: ["co2","lat", "lon"])')
-
+                                    '|> keep(columns: ["co2", "lat", "lon"])')
 
 map_fig = go.Figure(data=go.Scattermapbox(
     lon=map_df['lon'],
@@ -98,17 +104,19 @@ app.clientside_callback(
     [
         Input('timezone', 'children'),
         Input('duration', 'value'),
+        Input('host', 'value'),
         Input('interval', 'n_intervals'),
     ],
 )
-def update_graphs(timezone, duration, n_intervals):
-    df = get_influxdb_data(duration)
+def update_graphs(timezone, duration, host, n_intervals):
+    df = get_influxdb_data(duration, host)
     df.columns = ['Time', 'Altitude', 'Barometric Pressure (mBar)', 'CO₂ (PPM)', 'Humidity (%)', 'Latitude', 'Longitude', 'Temperature (C)']
     df['Time'] = df['Time'].dt.tz_convert(timezone)
     co2_line = px.line(df, x='Time', y='CO₂ (PPM)', color_discrete_sequence=['black'], template='plotly_white', render_mode='svg')
     temp_line = px.line(df, x='Time', y='Temperature (C)', color_discrete_sequence=['black'], template='plotly_white', render_mode='svg')
     baro_line = px.line(df, x='Time', y='Barometric Pressure (mBar)', color_discrete_sequence=['black'], template='plotly_white', render_mode='svg')
     humidity_line = px.line(df, x='Time', y='Humidity (%)', color_discrete_sequence=['black'], template='plotly_white', render_mode='svg')
+    
     return co2_line, temp_line, baro_line, humidity_line
 
 # Export data as CSV
