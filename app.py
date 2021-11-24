120d4aabcc8f8b20ddfa246216e4adbc32d98e6a
Srikiran <srikiranc@gmail.com>
2021-11-24 12:15:17 -0500

    Add hover template for subplots

--- app.py
+++ app.py
@@ -225,7 +225,7 @@ def update_graphs(timezone, duration, click_feature, _n_intervals):
     columns_to_plot = ['CO₂ (PPM)', 'Temperature (°C)', 'Barometric Pressure (mBar)', 'Humidity (%)']
     fig = make_subplots(rows=4, cols=1, shared_xaxes=True)
     for ind, col in enumerate(columns_to_plot):
-        fig.add_scatter(x=sensor_data["Time"], y=sensor_data[col], mode="lines", line=go.scatter.Line(color="gray"), showlegend=False, row=ind+1, col=1)
+        fig.add_scatter(x=sensor_data["Time"], y=sensor_data[col], mode="lines", line=go.scatter.Line(color="black"), showlegend=False, row=ind+1, col=1, hovertemplate="Time: %{x}<br>%{text}: %{y:.2f}<extra></extra>", text=[col]*len(sensor_data[col]))
         fig.update_yaxes(title_text=col, row=ind+1, col=1)
     fig.update_layout(template="plotly_white", height=1200)
     return fig
