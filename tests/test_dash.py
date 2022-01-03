951d90facc87359d1aa216d2e93717174c50d132
Zoltan Nagy <abesto0@gmail.com>
2022-01-03 11:21:39 +0000

    Check and dismiss geolocation error alert

--- tests/test_dash.py
+++ tests/test_dash.py
@@ -5,4 +5,12 @@ def test_dash001_smoke(dash_duo):
     dash_duo.start_server(app)
 
     dash_duo.wait_for_element("h1")
+
+    # Dismiss "User denied Geolocation" alert
+    driver = dash_duo.driver
+    alert = driver.switch_to.alert
+    assert alert.text == "Geolocation error: User denied Geolocation."
+    alert.accept()
+
+    # Simple verification that the page loaded (beyond the geolocation alert)
     assert dash_duo.find_element("h1").text == "Ribbit Network"
