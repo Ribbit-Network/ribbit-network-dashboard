8a6652a14ac86976e320fc7ae7bd8e5630a506f0
fosteman <timfosteman@gmail.com>
2022-08-07 00:01:27 -0400

    env variables

--- functions/src/core.ts
+++ functions/src/core.ts
@@ -17,8 +17,8 @@ class Core {
   constructor() {
     this.influxDB = new InfluxDB({
       url: "https://us-west-2-1.aws.cloud2.influxdata.com",
-      token: process.env.influxDBToken,
-    }).getQueryApi("keenan.johnson@gmail.com");
+      token: process.env.INFLUX_DB_TOKEN,
+    }).getQueryApi(process.env.INFLUX_DB_ORG);
 
     makeAutoObservable(this);
   }
