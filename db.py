0f7c0a3191b7125523329c9098b63de33028f4cd
Keenan Johnson <keenan.johnson@gmail.com>
2022-11-26 16:46:53 -0800

    #105 Switched InfluxDB Account and Bucket

--- db.py
+++ db.py
@@ -9,7 +9,7 @@ query_api = client.query_api()
 
 @cached(cache=TTLCache(maxsize=1, ttl=60))
 def get_map_data() -> pd.DataFrame:
-    df = query_api.query_data_frame('from(bucket:"co2")'
+    df = query_api.query_data_frame('from(bucket:"lab-testing")'
                                     '|> range(start:-30d)'
                                     '|> filter(fn: (r) => r._field == "co2" or r._field == "lat" or r._field == "lon")'
                                     '|> last()'
@@ -25,7 +25,7 @@ def get_map_data() -> pd.DataFrame:
 # `100` chosen arbitrarily, tweak with extreme prejudice
 @cached(cache=TTLCache(maxsize=100, ttl=60))
 def get_sensor_data(host: str, duration: str, frequency: str) -> pd.DataFrame:
-    df = query_api.query_data_frame('from(bucket:"co2")'
+    df = query_api.query_data_frame('from(bucket:"lab-testing")'
                                     f'|> range(start: -{duration})'
                                     f'|> filter(fn: (r) => r.host == "{host}")'
                                     '|> aggregateWindow(every: 1m, fn: mean, createEmpty: false)'
